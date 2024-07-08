7884cdf851cdd947c9530550ec7fbb4e264b9835
vlopez2828 <vickylopez2828@gmail.com>
2024-07-08 02:11:09 -0300

    cambios en index.js

--- index.js
+++ index.js
@@ -26,42 +26,42 @@ const upload = multer({ storage });
 const path = require('path');
 app.use('/static', express.static(path.join(__dirname, './upload')));
 
-app.get('/', (req, res)=>{
-  const htmlResponse =
-    `<html>
-      <head>MUNDO GAMING</head>
-      <body>
-        <h1>Holaaaa mundo gaming</h1>
-      </body>
-    </html>`;
-    res.send(htmlResponse);
-});
+// app.get('/', (req, res)=>{
+//   const htmlResponse =
+//     `<html>
+//       <head>MUNDO GAMING</head>
+//       <body>
+//         <h1>Holaaaa mundo gaming</h1>
+//       </body>
+//     </html>`;
+//     res.send(htmlResponse);
+// });
 //ruteo games
 
- //app.get('/',  GamesController.getAllGames);
-// app.get('/api/games/:id',  GamesController.getGame);
-// app.post('/api/games', upload.single('gameImage'), (req, res) => {
-//     //console.log(req.file)
-//     GamesController.insertGame(req, res);
-// } );
-// app.put('/api/games/:id', upload.single('gameImage'), (req, res) => {
-//  console.log(req.file, "put")
-//   GamesController.updateGame(req, res);
-// } );
-// app.delete('/api/games/:id', GamesController.deleteGame);
+ app.get('/',  GamesController.getAllGames);
+app.get('/api/games/:id',  GamesController.getGame);
+app.post('/api/games', upload.single('gameImage'), (req, res) => {
+    //console.log(req.file)
+    GamesController.insertGame(req, res);
+} );
+app.put('/api/games/:id', upload.single('gameImage'), (req, res) => {
+ console.log(req.file, "put")
+  GamesController.updateGame(req, res);
+} );
+app.delete('/api/games/:id', GamesController.deleteGame);
 
-// app.post('/api/images', upload.single('gameImage'), (req, res)=>{
-//   console.log(req.file);
-//   res.send("okk")
-// });
+app.post('/api/images', upload.single('gameImage'), (req, res)=>{
+  console.log(req.file);
+  res.send("okk")
+});
 
 //ruteo user
-// app.get('/api/users',  UsersController.getAllUsers);
-// app.get('/api/users/:id',  UsersController.getUser);
-// app.post('/api/users', UsersController.newUser);
-// app.put('/api/users/:id', UsersController.updateUser)
-// app.delete('/api/users/:id', UsersController.deleteUser);
-// app.post('/api/users/login', UsersController.loginUser);
+app.get('/api/users',  UsersController.getAllUsers);
+app.get('/api/users/:id',  UsersController.getUser);
+app.post('/api/users', UsersController.newUser);
+app.put('/api/users/:id', UsersController.updateUser)
+app.delete('/api/users/:id', UsersController.deleteUser);
+app.post('/api/users/login', UsersController.loginUser);
 
 app.listen(port, () => {
   console.log(`Escuchando en el puerto: ${port}`)
